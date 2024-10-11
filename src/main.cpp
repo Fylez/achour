0cbc1c4c6481ff3d571c95d1b55ed90f063a3e5e
Fylez <superliamzaengel@gmail.com>
2024-10-11 21:01:48 +0200

    changed some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,19 +6,17 @@ int getHour()
 {
 	std::time_t currentTime;
 	std::tm *timezone;
-	int currentHour;
 
 	currentTime = std::time(NULL);
 	timezone = std::localtime(&currentTime);
-	currentHour = timezone->tm_hour;
 	
-	return(currentHour);
+	return(timezone->tm_hour);
 }
 
 int	main(void){
 
 	sf::Music music;
-	std::string sunnyFiles[] = {"0.flac", "1.flac", "2.flac", "3.flac", "4.flac", "5.flac", "6.flac" ,"7.flac" ,"8.flac" ,"9.flac" ,"10.flac", "11.flac" ,"12.flac", "13.flac", "14.flac", "15.flac", "16.flac", "17.flac", "18.flac" ,"19.flac" ,"20.flac" ,"21.flac" ,"22.flac", "23.flac"};
+	//std::string sunnyFiles[] = {"0.flac", "1.flac", "2.flac", "3.flac", "4.flac", "5.flac", "6.flac" ,"7.flac" ,"8.flac" ,"9.flac" ,"10.flac", "11.flac" ,"12.flac", "13.flac", "14.flac", "15.flac", "16.flac", "17.flac", "18.flac" ,"19.flac" ,"20.flac" ,"21.flac" ,"22.flac", "23.flac"};
 	int hour;
 	while(1)
 	{
@@ -28,7 +26,7 @@ int	main(void){
 	
 			if(music.getStatus() == sf::SoundSource::Stopped)
 			{
-					if (!music.openFromFile(sunnyFiles[hour]))
+					if (!music.openFromFile("sunny/" + std::to_string(hour) + ".flac"))
 					{
 						std::cout << "error" << std::endl;
 						return -1; // error
