ea5b27d93fbe6ee7324e61e6bf505fa533dad0c7
Fylez <superliamzaengel@gmail.com>
2024-10-11 22:33:22 +0200

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,6 @@ int getHour()
 int	main(void){
 
 	sf::Music music;
-	//std::string sunnyFiles[] = {"0.flac", "1.flac", "2.flac", "3.flac", "4.flac", "5.flac", "6.flac" ,"7.flac" ,"8.flac" ,"9.flac" ,"10.flac", "11.flac" ,"12.flac", "13.flac", "14.flac", "15.flac", "16.flac", "17.flac", "18.flac" ,"19.flac" ,"20.flac" ,"21.flac" ,"22.flac", "23.flac"};
 	int hour;
 	while(1)
 	{
@@ -29,7 +28,7 @@ int	main(void){
 					if (!music.openFromFile("sunny/" + std::to_string(hour) + ".flac"))
 					{
 						std::cout << "error" << std::endl;
-						return -1; // error
+						return -1;
 					}
 					std::cout << "Playing " << "sunny " << "variant for " << hour << std::endl;
 					music.play();
