bc1831598887918b513b8a57a1c9ee919ab602ae
Fylez <superliamzaengel@gmail.com>
2024-10-11 14:33:41 +0200

    First version work with hour, now weather

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,45 @@
-#include <SFML/Graphics.hpp>
+#include <ctime>
+#include <SFML/Audio.hpp>
+#include <iostream>
 
-int main()
+int getHour()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+	std::time_t currentTime;
+	std::tm *timezone;
+	int currentHour;
 
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
+	currentTime = std::time(NULL);
+	timezone = std::localtime(&currentTime);
+	currentHour = timezone->tm_hour;
+	
+	return(currentHour);
+}
 
-        window.clear();
-        window.display();
-    }
+int	main(void){
+
+	sf::Music music;
+	std::string sunnyFiles[] = {"0.flac", "1.flac", "2.flac", "3.flac", "4.flac", "5.flac", "6.flac" ,"7.flac" ,"8.flac" ,"9.flac" ,"10.flac", "11.flac" ,"12.flac", "13.flac", "14.flac", "15.flac", "16.flac", "17.flac", "18.flac" ,"19.flac" ,"20.flac" ,"21.flac" ,"22.flac", "23.flac"};
+	int hour;
+	while(1)
+	{
+		hour = getHour();
+		while(hour == getHour())
+		{
+	
+			if(music.getStatus() == sf::SoundSource::Stopped)
+			{
+					if (!music.openFromFile(sunnyFiles[hour]))
+					{
+						std::cout << "error" << std::endl;
+						return -1; // error
+					}
+					std::cout << "Playing " << "sunny " << "variant for " << hour << std::endl;
+					music.play();
+				
+			}
+			sf::sleep(sf::seconds(3));
+		}
+		if(music.getStatus() == sf::SoundSource::Playing)
+			music.stop();
+	}
 }
